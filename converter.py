b1b12780c8ceb9d91db5f64796fb7250bb170f51
guramiko <9.fircxalaishvili@gmail.com>
2025-04-28 08:33:44 +0400

    added basic functions

---
+++ converter.py
@@ -0,0 +1,13 @@
+def convert(ops, value):
+    if ops == "km":
+        return f"Metre: {value * 1000}"
+    elif ops == "m":
+        return f"KM: {value / 1000}"
+    else:
+        return "ERROR"
+    
+ops = input("KM or M? ")
+value = int(input("value: "))
+
+print(convert(ops, value))
+
